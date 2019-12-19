ccbabc6d10e0a0b67f5286705a09fcb2452533bc
JN94 <jessicananne94@hotmail.com>
2019-12-19 16:16:18 +0100

    terugzetten

--- screens/Index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Screen from './Screen';
-
-export const JournalScreen = () => <Screen />;
-export const MeasureScreen = () => <Screen />;
-export const TreatmentScreen = () => <Screen />;
-export const ProfileScreen = () => <Screen />;
